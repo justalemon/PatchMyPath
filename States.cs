a21f132e8e9c2cd609fab93421e63114b82c20b6
Lemon <justlemoncl@gmail.com>
2018-01-21 20:58:26 -0300

    Added file and install type enums

---
+++ States.cs
@@ -0,0 +1,16 @@
+﻿namespace PatchMyPath
+{
+    public enum InstallType : int
+    {
+        Clean = 1,
+        Modded = 2,
+        Fast = 3
+    }
+
+    public enum FileType : int
+    {
+        FoundNotGame = 1,
+        FoundIsGame = 2,
+        NotFound = 3
+    }
+}
