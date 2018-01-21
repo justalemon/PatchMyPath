eca569ea2e5079444191a25886f0b0e00ae332a6
Lemon <justlemoncl@gmail.com>
2018-01-21 20:50:33 -0300

    Check if the line was already set

--- Program.cs
+++ Program.cs
@@ -11,33 +11,43 @@ namespace PatchMyPath
     {
         static void Main(string[] args)
         {
-            string LocalDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             string RegKey = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Rockstar Games\\Grand Theft Auto V";
             string RegValue = "InstallFolder";
+
+            string LocalDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string SavedDir = (string)Registry.GetValue(RegKey, RegValue, string.Empty);
+
             bool ValidDir = true;
             int ExitCode = 0;
+
             List<string> Executables = new List<string>() {"GTA5.exe", "GTAVLauncher.exe", "PlayGTAV.exe"};
 
             Console.WriteLine("Local Path is " + LocalDir);
+            Console.WriteLine("Saved Path is " + LocalDir);
             Console.WriteLine("Registry Key is " + RegKey);
             Console.WriteLine();
-
-            foreach (var Executable in Executables)
-            {
-                if (!File.Exists(LocalDir + "\\" + Executable))
+            
+                foreach (var Executable in Executables)
                 {
-                    Console.WriteLine(Executable + " was not found.");
-                    ValidDir = false;
+                    if (!File.Exists(LocalDir + "\\" + Executable))
+                    {
+                        Console.WriteLine(Executable + " was not found.");
+                        ValidDir = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine(Executable + " was found.");
+                    }
                 }
-                else
-                {
-                    Console.WriteLine(Executable + " was found.");
-                }
-            }
 
             Console.WriteLine();
-
-            if (ValidDir)
+            
+            if (LocalDir == SavedDir)
+            {
+                Console.WriteLine("The directory is the same.");
+                ExitCode = 0;
+            }
+            else if (ValidDir)
             {
                 Registry.SetValue(RegKey, RegValue, LocalDir);
                 Console.WriteLine("GTA V Dir was set to: " + LocalDir);
