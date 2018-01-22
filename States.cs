c1ce0a6be4813f1a5fbfc906c7e9d00c34918214
Lemon <justlemoncl@gmail.com>
2018-01-21 21:15:21 -0300

    Removed unused enum

--- States.cs
+++ States.cs
@@ -1,12 +1,5 @@
 ﻿namespace PatchMyPath
 {
-    public enum InstallType : int
-    {
-        Clean = 1,
-        Modded = 2,
-        Fast = 3
-    }
-
     public enum FileType : int
     {
         FoundNotGame = 1,
