f4a91387302e05b2eb05e4861077c8926d50705c
Lemon <justlemoncl@gmail.com>
2018-01-21 21:00:06 -0300

    Added a file check

---
+++ Checks.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Diagnostics;
+
+namespace PatchMyPath
+{
+    static class Checks
+    {
+        public static FileType CheckGameFile(string FileLocation)
+        {
+            bool Exists = File.Exists(FileLocation);
+            string Product = string.Empty;
+            FileVersionInfo FileInfo;
+
+            if (Exists)
+            {
+                FileInfo = FileVersionInfo.GetVersionInfo(FileLocation);
+                Product = FileInfo.ProductName;
+            }
+
+            if (Exists && Product == "Grand Theft Auto V")
+                return FileType.FoundIsGame;
+            else if (Exists)
+                return FileType.FoundNotGame;
+            else
+                return FileType.NotFound;
+        }
+    }
+}
