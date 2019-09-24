0cde97568b793b52e934270153bfba266356b749
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-24 14:40:15 -0300

    Added basic .NET Core Console App

---
+++ PatchMyPath/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PatchMyPath
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+}
