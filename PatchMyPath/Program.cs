71da35fc7fe96e58ec17bd93703c2add47f8e1a8
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-26 13:39:39 -0300

    Added parsing of the configuration file

--- PatchMyPath/Program.cs
+++ PatchMyPath/Program.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace PatchMyPath
 {
     public class Program
     {
+        /// <summary>
+        /// The configuration of the program.
+        /// </summary>
+        public static Configuration Config = null;
+
         public static int Main(string[] args)
         {
+            // Try to parse the configuration
+            try
+            {
+                Config = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("PatchMyPath.json"));
+            }
+            // If the file was not found
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The Configuration File does not exists!");
+                Console.WriteLine("Please check that PatchMyPath.json is present and try again.");
+                return 1;
+            }
+            // If the file could not be parsed
+            catch (JsonSerializationException)
+            {
+                Console.WriteLine("The Configuration file is invalid!");
+                Console.WriteLine("Please make sure that the Curly Brackets and Comas are in the correct place.");
+                Console.WriteLine("If you know what you are doing: Use a Linter on a Code Editor/IDE.");
+                return 2;
+            }
+
             Console.WriteLine("Hello World!");
             return 0;
         }
