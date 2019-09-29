ecd4f52ec13fdcb244dd1542efd796778c230bc7
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-28 21:51:36 -0300

    Added symbolic link processing and sanity checks

--- PatchMyPath/Program.cs
+++ PatchMyPath/Program.cs
@@ -51,10 +51,20 @@ namespace PatchMyPath
             Console.WriteLine("The following game installs are available:");
             Console.WriteLine();
 
-            // Print all of the available installs
+            // Iterate over the number of game installs
             for (int i = 0; i < Config.GameInstalls.Count; i++)
             {
-                Console.WriteLine($"{i}: {Config.GameInstalls[i].GamePath} [Valid: {Config.GameInstalls[i].IsLegal}]");
+                // Save the install
+                Install selected = Config.GameInstalls[i];
+
+                // If the install is set to auto detect, update the type
+                if (selected.Type == InstallType.AutoDetect)
+                {
+                    selected.UpdateType(Config.UseSteam);
+                }
+
+                // And print it on the console
+                Console.WriteLine($"{i}: {selected.GamePath} [Valid: {selected.IsLegal}] [Type: {selected.Type}]");
             }
 
             // And request the user to input a number
@@ -68,6 +78,31 @@ namespace PatchMyPath
             {
                 Console.WriteLine($"'{input}' is not a valid number! Exiting...");
                 return 3;
+            }
+
+            // If the index is over the limit
+            if (output > Config.GameInstalls.Count - 1)
+            {
+                Console.WriteLine($"'{output}' is outside of the range 0-{Config.GameInstalls.Count - 1}! Exiting...");
+                return 4;
+            }
+
+            // At this point we have a valid install number
+            // So save it here for later
+            Install install = Config.GameInstalls[output];
+
+            // Now, destroy the original game folder if is present
+            if (Directory.Exists(Config.Destination))
+            {
+                Directory.Delete(Config.Destination);
+            }
+
+            // Then, create a symbolic link between the game and the executable
+            // If we didn't succeeded, notify the user and exit
+            if (!CreateSymbolicLink(Config.Destination, install.GamePath, 3)) // 3 means Directory (0x1) and Unprivileged/Dev Mode (0x2)
+            {
+                Console.WriteLine($"Error while creating the Symbolic Link! Exiting...");
+                return 5;
             }
 
             // If we got here, success!
