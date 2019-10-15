f05aaec310447f82e17014836520e1772e85db53
Hannele Ruiz <justlemoncl@gmail.com>
2019-10-15 14:06:12 -0300

    Added basic set of UI Elements

--- InstallDuplicator/Home.Designer.cs
+++ InstallDuplicator/Home.Designer.cs
@@ -1,4 +1,4 @@
-namespace InstallDuplicator
+﻿namespace InstallDuplicator
 {
     partial class Home
     {
@@ -28,24 +28,108 @@ namespace InstallDuplicator
         /// </summary>
         private void InitializeComponent()
         {
+            this.OriginGroupBox = new System.Windows.Forms.GroupBox();
+            this.OriginTextBox = new System.Windows.Forms.TextBox();
+            this.OriginButton = new System.Windows.Forms.Button();
+            this.DestinationGroupBox = new System.Windows.Forms.GroupBox();
+            this.DestinationTextBox = new System.Windows.Forms.TextBox();
+            this.DestinationButton = new System.Windows.Forms.Button();
+            this.OriginGroupBox.SuspendLayout();
+            this.DestinationGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // OriginGroupBox
+            // 
+            this.OriginGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OriginGroupBox.Controls.Add(this.OriginButton);
+            this.OriginGroupBox.Controls.Add(this.OriginTextBox);
+            this.OriginGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.OriginGroupBox.Name = "OriginGroupBox";
+            this.OriginGroupBox.Size = new System.Drawing.Size(460, 45);
+            this.OriginGroupBox.TabIndex = 0;
+            this.OriginGroupBox.TabStop = false;
+            this.OriginGroupBox.Text = "Origin";
+            // 
+            // OriginTextBox
+            // 
+            this.OriginTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OriginTextBox.Location = new System.Drawing.Point(6, 19);
+            this.OriginTextBox.Name = "OriginTextBox";
+            this.OriginTextBox.ReadOnly = true;
+            this.OriginTextBox.Size = new System.Drawing.Size(367, 20);
+            this.OriginTextBox.TabIndex = 0;
+            // 
+            // OriginButton
+            // 
+            this.OriginButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OriginButton.Location = new System.Drawing.Point(379, 17);
+            this.OriginButton.Name = "OriginButton";
+            this.OriginButton.Size = new System.Drawing.Size(75, 23);
+            this.OriginButton.TabIndex = 1;
+            this.OriginButton.Text = "Select";
+            this.OriginButton.UseVisualStyleBackColor = true;
+            // 
+            // DestinationGroupBox
+            // 
+            this.DestinationGroupBox.Controls.Add(this.DestinationButton);
+            this.DestinationGroupBox.Controls.Add(this.DestinationTextBox);
+            this.DestinationGroupBox.Location = new System.Drawing.Point(12, 63);
+            this.DestinationGroupBox.Name = "DestinationGroupBox";
+            this.DestinationGroupBox.Size = new System.Drawing.Size(460, 45);
+            this.DestinationGroupBox.TabIndex = 1;
+            this.DestinationGroupBox.TabStop = false;
+            this.DestinationGroupBox.Text = "Destination";
+            // 
+            // DestinationTextBox
+            // 
+            this.DestinationTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DestinationTextBox.Location = new System.Drawing.Point(6, 19);
+            this.DestinationTextBox.Name = "DestinationTextBox";
+            this.DestinationTextBox.ReadOnly = true;
+            this.DestinationTextBox.Size = new System.Drawing.Size(367, 20);
+            this.DestinationTextBox.TabIndex = 0;
+            // 
+            // DestinationButton
+            // 
+            this.DestinationButton.Location = new System.Drawing.Point(379, 17);
+            this.DestinationButton.Name = "DestinationButton";
+            this.DestinationButton.Size = new System.Drawing.Size(75, 23);
+            this.DestinationButton.TabIndex = 1;
+            this.DestinationButton.Text = "Select";
+            this.DestinationButton.UseVisualStyleBackColor = true;
+            // 
+            // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 161);
+            this.Controls.Add(this.DestinationGroupBox);
+            this.Controls.Add(this.OriginGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Form1";
+            this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PatchMyPath Install Duplicator";
+            this.OriginGroupBox.ResumeLayout(false);
+            this.OriginGroupBox.PerformLayout();
+            this.DestinationGroupBox.ResumeLayout(false);
+            this.DestinationGroupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox OriginGroupBox;
+        private System.Windows.Forms.TextBox OriginTextBox;
+        private System.Windows.Forms.Button OriginButton;
+        private System.Windows.Forms.GroupBox DestinationGroupBox;
+        private System.Windows.Forms.TextBox DestinationTextBox;
+        private System.Windows.Forms.Button DestinationButton;
     }
 }
 
