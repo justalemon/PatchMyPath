2140d8f05cb5490c7c78464f8b4f4c4d1acf0534
Hannele Ruiz <justlemoncl@gmail.com>
2019-10-15 13:49:10 -0300

    Changed some basic settings of the form

--- InstallDuplicator/Form1.Designer.cs
+++ InstallDuplicator/Form1.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 161);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PatchMyPath Install Duplicator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
