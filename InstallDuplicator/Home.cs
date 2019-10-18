1c857b175c1eb7ad969ec7db31e3a594732ead42
Hannele Ruiz <justlemoncl@gmail.com>
2019-10-18 13:07:04 -0300

    Added list of GTA V Files

--- InstallDuplicator/Home.cs
+++ InstallDuplicator/Home.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +12,72 @@ namespace InstallDuplicator
 {
     public partial class Home : Form
     {
+        /// <summary>
+        /// The folders required for the game.
+        /// </summary>
+        public static string[] RequiredFolders = new string[]
+        {
+            "update",
+            "x64",
+        };
+        /// <summary>
+        /// Files that are required for running the game.
+        /// </summary>
+        public static string[] RequiredFiles = new string[]
+        {
+            "GTA5.exe",
+            "GTAVLauncher.exe",
+            "bink2w64.dll",
+            "d3dcompiler_46.dll",
+            "d3dcsx_46.dll",
+            "GFSDK_ShadowLib.win64.dll",
+            "GFSDK_TXAA.win64.dll",
+            "GFSDK_TXAA_AlphaResolve.win64.dll",
+            "GPUPerfAPIDX11-x64.dll",
+            "NvPmApi.Core.win64.dll",
+            "common.rpf",
+            "x64a.rpf",
+            "x64b.rpf",
+            "x64c.rpf",
+            "x64d.rpf",
+            "x64e.rpf",
+            "x64f.rpf",
+            "x64g.rpf",
+            "x64h.rpf",
+            "x64i.rpf",
+            "x64j.rpf",
+            "x64k.rpf",
+            "x64l.rpf",
+            "x64m.rpf",
+            "x64n.rpf",
+            "x64o.rpf",
+            "x64p.rpf",
+            "x64q.rpf",
+            "x64r.rpf",
+            "x64s.rpf",
+            "x64t.rpf",
+            "x64u.rpf",
+            "x64v.rpf",
+            "x64w.rpf",
+        };
+        /// <summary>
+        /// Folders that contain files that are not required by the game.
+        /// </summary>
+        public static string[] OptionalFolders = new string[]
+        {
+            "ReadMe",
+            "Redistributables",
+        };
+        /// <summary>
+        /// Files that have been added on recent updates and are not mandatory.
+        /// </summary>
+        public static string[] OptionalFiles = new string[]
+        {
+            "PlayGTAV.exe",
+            "index.bin",
+            "version.txt",
+        };
+
         public Home()
         {
             // Initialize the UI elements
