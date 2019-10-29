a22930fb08128ed7486966a01c0ebe8e36362710
Hannele Ruiz <justlemoncl@gmail.com>
2019-10-29 02:52:50 -0300

    Ask the user if he wants to open the new GTA V folder

--- InstallDuplicator/Home.cs
+++ InstallDuplicator/Home.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
@@ -197,6 +198,14 @@ namespace InstallDuplicator
                     LogTextBox.AppendText($"ERROR: {er.Message}{Environment.NewLine}");
                     return;
                 }
+            }
+
+            // Finally, notify the user that we completed the process and ask him if he wants to open the folder
+            DialogResult shouldOpen = MessageBox.Show("The duplication process has been completed!\nDo you want to open the new GTA V Folder?", "Duplication Complete", MessageBoxButtons.YesNo);
+            // If he does, open it up on the explorer
+            if (shouldOpen == DialogResult.Yes)
+            {
+                Process.Start(DestinationTextBox.Text);
             }
         }
     }
