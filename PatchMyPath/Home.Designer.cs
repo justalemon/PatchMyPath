1c3f27daadd1ec30cccfdd4aad0e228a6125673e
Hannele Ruiz <justlemoncl@gmail.com>
2019-10-30 01:59:28 -0300

    Added basic controls for the operation of the program

--- PatchMyPath/Home.Designer.cs
+++ PatchMyPath/Home.Designer.cs
@@ -1,4 +1,4 @@
-namespace PatchMyPath
+﻿namespace PatchMyPath
 {
     partial class Home
     {
@@ -28,21 +28,115 @@ namespace PatchMyPath
         /// </summary>
         private void InitializeComponent()
         {
+            this.GeneralTabControl = new System.Windows.Forms.TabControl();
+            this.InstallsTabPage = new System.Windows.Forms.TabPage();
+            this.DuplicatorTabPage = new System.Windows.Forms.TabPage();
+            this.InstallsGroupBox = new System.Windows.Forms.GroupBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.LaunchButton = new System.Windows.Forms.Button();
+            this.GeneralTabControl.SuspendLayout();
+            this.InstallsTabPage.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // GeneralTabControl
+            // 
+            this.GeneralTabControl.Controls.Add(this.InstallsTabPage);
+            this.GeneralTabControl.Controls.Add(this.DuplicatorTabPage);
+            this.GeneralTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GeneralTabControl.Location = new System.Drawing.Point(0, 0);
+            this.GeneralTabControl.Name = "GeneralTabControl";
+            this.GeneralTabControl.SelectedIndex = 0;
+            this.GeneralTabControl.Size = new System.Drawing.Size(334, 461);
+            this.GeneralTabControl.TabIndex = 0;
+            // 
+            // InstallsTabPage
+            // 
+            this.InstallsTabPage.Controls.Add(this.LaunchButton);
+            this.InstallsTabPage.Controls.Add(this.DeleteButton);
+            this.InstallsTabPage.Controls.Add(this.AddButton);
+            this.InstallsTabPage.Controls.Add(this.InstallsGroupBox);
+            this.InstallsTabPage.Location = new System.Drawing.Point(4, 22);
+            this.InstallsTabPage.Name = "InstallsTabPage";
+            this.InstallsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.InstallsTabPage.Size = new System.Drawing.Size(326, 435);
+            this.InstallsTabPage.TabIndex = 0;
+            this.InstallsTabPage.Text = "Install Manager";
+            this.InstallsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // DuplicatorTabPage
+            // 
+            this.DuplicatorTabPage.Location = new System.Drawing.Point(4, 22);
+            this.DuplicatorTabPage.Name = "DuplicatorTabPage";
+            this.DuplicatorTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.DuplicatorTabPage.Size = new System.Drawing.Size(326, 435);
+            this.DuplicatorTabPage.TabIndex = 1;
+            this.DuplicatorTabPage.Text = "Duplicator";
+            this.DuplicatorTabPage.UseVisualStyleBackColor = true;
+            // 
+            // InstallsGroupBox
+            // 
+            this.InstallsGroupBox.Location = new System.Drawing.Point(8, 6);
+            this.InstallsGroupBox.Name = "InstallsGroupBox";
+            this.InstallsGroupBox.Size = new System.Drawing.Size(310, 100);
+            this.InstallsGroupBox.TabIndex = 0;
+            this.InstallsGroupBox.TabStop = false;
+            this.InstallsGroupBox.Text = "Available Installs";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AddButton.Location = new System.Drawing.Point(8, 404);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DeleteButton.Location = new System.Drawing.Point(89, 404);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 2;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // LaunchButton
+            // 
+            this.LaunchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.LaunchButton.Location = new System.Drawing.Point(243, 404);
+            this.LaunchButton.Name = "LaunchButton";
+            this.LaunchButton.Size = new System.Drawing.Size(75, 23);
+            this.LaunchButton.TabIndex = 3;
+            this.LaunchButton.Text = "Launch";
+            this.LaunchButton.UseVisualStyleBackColor = true;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(334, 461);
+            this.Controls.Add(this.GeneralTabControl);
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "PatchMyPath";
+            this.GeneralTabControl.ResumeLayout(false);
+            this.InstallsTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl GeneralTabControl;
+        private System.Windows.Forms.TabPage InstallsTabPage;
+        private System.Windows.Forms.TabPage DuplicatorTabPage;
+        private System.Windows.Forms.GroupBox InstallsGroupBox;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button LaunchButton;
     }
 }
 
