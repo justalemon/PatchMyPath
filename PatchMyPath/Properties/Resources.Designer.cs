94e7cb6a3a41eb21b74ea989e8720cc2fcd701ff
Hannele Ruiz <justlemoncl@gmail.com>
2019-11-03 02:34:19 -0300

    Added Image/Icon for the Start button

--- PatchMyPath/Properties/Resources.Designer.cs
+++ PatchMyPath/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace PatchMyPath.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Start {
+            get {
+                object obj = ResourceManager.GetObject("Start", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
