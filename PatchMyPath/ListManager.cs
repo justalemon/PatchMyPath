16a490f6d2d4f9d1bff1a02b347880e2788f857f
Hannele Ruiz <justlemoncl@gmail.com>
2020-02-28 06:54:42 -0300

    Moved EntryType Enum into the ListManager class

--- PatchMyPath/ListManager.cs
+++ PatchMyPath/ListManager.cs
@@ -11,6 +11,18 @@ using System.Net;
 namespace PatchMyPath
 {
     /// <summary>
+    /// The type of filesystem entry for the game content.
+    /// </summary>
+    [Flags]
+    public enum EntryType
+    {
+        File = 1,
+        Folder = 2,
+        Optional = 4,
+        Copy = 8,
+    }
+
+    /// <summary>
     /// Manager for the File Lists.
     /// </summary>
     public static class ListManager
