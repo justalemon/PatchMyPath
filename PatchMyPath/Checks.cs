52c046f77ed6e7ad4ef98d3b2e9ad7b43e2eaeb2
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-13 15:36:30 -0400

    Added check for the newer Rockstar Games certificate

--- PatchMyPath/Checks.cs
+++ PatchMyPath/Checks.cs
@@ -16,7 +16,8 @@ namespace PatchMyPath
         /// <returns>true if the file exists and has a valid signature, false otherwise.</returns>
         public static bool IsFileSignedByRockstar(string file)
         {
-            // This is the basic information of the Rockstar Games certificate:
+            // This is the basic information of the Rockstar Games certificates:
+
             // Issued to: Rockstar Games, Inc.
             // Issues by: Entrust Code Signing CA - OVCS1
             // Serial number: 00e49e47111fec98cd0000000055662b3e
@@ -24,6 +25,14 @@ namespace PatchMyPath
             // Valid from: ‎Thursday, ‎March ‎9, ‎2017 7:04:49 PM
             // Valid to: ‎Friday, ‎March ‎20, ‎2020 7:34:46 PM
             // Thumbprint/Hash: aa0d31a9c8c1ebd9e18ec1d8d3f98b3523178ad8
+
+            // Issued to: Rockstar Games, Inc.
+            // Issues by: DigiCert SHA2 Assured ID Code Signing CA
+            // Serial number: 0f65f4572517cbccaa8b3776580a8d3d
+            // Algorithm: sha256RSA
+            // Valid from: ‎Thursday, ‎February ‎6, ‎2020 8:00:00 PM
+            // Valid to: ‎Friday, ‎February ‎17, ‎2023 8:00:00 AM
+            // Thumbprint/Hash: 0ebf58d74ccabca4fce1ab83adba62b40dfc014a
 
             // If the file does not exists, return
             if (!File.Exists(file))
@@ -53,7 +62,8 @@ namespace PatchMyPath
             }
 
             // Then finally, check if the file is signed by Rockstar Games
-            return certificate.GetCertHashString() == "AA0D31A9C8C1EBD9E18EC1D8D3F98B3523178AD8";
+            string hash = certificate.GetCertHashString();
+            return hash == "AA0D31A9C8C1EBD9E18EC1D8D3F98B3523178AD8" || hash == "0EBF58D74CCABCA4FCE1AB83ADBA62B40DFC014A";
         }
     }
 }
