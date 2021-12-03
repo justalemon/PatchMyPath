323b73c1628972c580fd3b0d3773d8cc29aeaea7
Hannele Ruiz <justlemoncl@gmail.com>
2021-12-02 23:10:06 -0300

    Added list of launch parameters for CLI launch

---
+++ PatchMyPath/LaunchParameters.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace PatchMyPath
+{
+    /// <summary>
+    /// The launch parameters used to start a folder from the command line.
+    /// </summary>
+    public class LaunchParameters
+    {
+        [Option("launch", Required = true, HelpText = "The installation to launch. It needs to be added to the program before starting unless --add is set.")]
+        public string Launch { get; set; }
+        [Option("add", Required = false, HelpText = "Adds the installation to the list if is not present.")]
+        public bool Add { get; set; }
+    }
+}
