67d301e9a908a94aca3af4e604a2bc7d94a1c61e
Hannele Ruiz <justlemoncl@gmail.com>
2023-07-12 00:25:58 -0400

    Removed invalid characters

--- PatchMyPath/Tools/Signatures.cs
+++ PatchMyPath/Tools/Signatures.cs
@@ -18,9 +18,9 @@ namespace PatchMyPath.Tools
             // Issued to: None
             // Issues by: None
             // Serial number: 0c56410e7a8cf26e7f1f306ffce670cc
-            // Algorith: sha1RSA
-            // Valid from: ‎Sunday, ‎September ‎21, ‎2008 8:00:00 PM
-            // Valid to: ‎Tuesday, ‎September ‎22, ‎2009 7:59:59 PM
+            // Algorithm: sha1RSA
+            // Valid from: Sunday, September 21, 2008 8:00:00 PM
+            // Valid to: Tuesday, September 22, 2009 7:59:59 PM
             // Thumbprint/Hash: f50405ad2d00444c13eb132fd8f00a6493e76521
             // Found in: GTA IV Day 1 Release
             "F50405AD2D00444C13EB132FD8F00A6493E76521",
@@ -28,24 +28,24 @@ namespace PatchMyPath.Tools
             // Issues by: Entrust Code Signing CA - OVCS1
             // Serial number: 5057286e4033fcb0000000005565f5ac
             // Algorithm: sha256RSA
-            // Valid from: ‎Friday, ‎March ‎4, ‎2016 5:46:15 PM
-            // Valid to: ‎Monday, ‎March ‎20, ‎2017 6:16:13 PM
+            // Valid from: Friday, March 4, 2016 5:46:15 PM
+            // Valid to: Monday, March 20, 2017 6:16:13 PM
             // Thumbprint/Hash: c9793f4a2e629d88f2213622d7a0c170d9c7cbc6
             "C9793F4A2E629D88F2213622D7A0C170D9C7CBC6",
             // Issued to: Rockstar Games, Inc.
             // Issues by: Entrust Code Signing CA - OVCS1
             // Serial number: 00e49e47111fec98cd0000000055662b3e
             // Algorithm: sha256RSA
-            // Valid from: ‎Thursday, ‎March ‎9, ‎2017 7:04:49 PM
-            // Valid to: ‎Friday, ‎March ‎20, ‎2020 7:34:46 PM
+            // Valid from: Thursday, March 9, 2017 7:04:49 PM
+            // Valid to: Friday, March 20, 2020 7:34:46 PM
             // Thumbprint/Hash: aa0d31a9c8c1ebd9e18ec1d8d3f98b3523178ad8
             "AA0D31A9C8C1EBD9E18EC1D8D3F98B3523178AD8",
             // Issued to: Rockstar Games, Inc.
             // Issues by: DigiCert SHA2 Assured ID Code Signing CA
             // Serial number: 0f65f4572517cbccaa8b3776580a8d3d
             // Algorithm: sha256RSA
-            // Valid from: ‎Thursday, ‎February ‎6, ‎2020 8:00:00 PM
-            // Valid to: ‎Friday, ‎February ‎17, ‎2023 8:00:00 AM
+            // Valid from: Thursday, February 6, 2020 8:00:00 PM
+            // Valid to: Friday, February 17, 2023 8:00:00 AM
             // Thumbprint/Hash: 0ebf58d74ccabca4fce1ab83adba62b40dfc014a
             "0EBF58D74CCABCA4FCE1AB83ADBA62B40DFC014A"
         };
